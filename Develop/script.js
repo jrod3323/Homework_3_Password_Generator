879fb79b83957f13e45a4028d73b0fb20ccb6c06
Jared Ackley <jrod3323@gmail.com>
2020-10-24 14:45:10 -0500

    code refined

--- Develop/script.js
+++ Develop/script.js
@@ -3,10 +3,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-
-
-
+  generatePassword();
 }
 
 // Add event listener to generate button
